06b71212a89ecc649e21931fbeedb6fa618277b9
Chris Julian <soapyking@gmail.com>
2012-05-01 19:45:33 -0400

    More work on driver to read in space-separated tokens

--- src/driver.java
+++ src/driver.java
@@ -3,31 +3,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.LinkedList; 
 
 public class driver {
 
-	LinkedList<Token> tokens;
+	HashSet<Token> tokens;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		File tokenFile = new File("tokens.tok");
-		File tableFile = new File("table.tok");
+		File tokenFile = new File("java_lexer/test_output.tok");
+		File tableFile = new File("java_lexer/parsetable.csv");
+		String line = null;
 		try {
 			FileReader reader = new FileReader(tokenFile);
 			BufferedReader buff = new BufferedReader(reader);
-			String line;
-			while((line = buff.readLine()) != null) {
-				Token token = new Token(line, null);
-			}
+			//This assumes there's only 1 line of tokens
+			//Glob help us all if there's more
+			line = buff.readLine();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			System.exit(1);
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.exit(1);
 		}
-		
-		
+		String[] splat = line.split(" ");
+		for(String s: splat) {
+			System.out.println(s);
+		}
 	}
-
 }
