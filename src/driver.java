cb960a7a08585692799616feb124e53e1debf3f7
Stewart Butler <stewart@ethosnet.net>
2012-05-01 23:04:00 -0400

    Parser table and a few other items
    Table isn't working, neither is the function to get it back from CSV.
    	modified:   run.sh
    	modified:   src/ParseTable.java
    	modified:   src/driver.java
    	deleted:    src/temp.tok

--- src/driver.java
+++ src/driver.java
@@ -5,43 +5,51 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList; 
+import java.util.LinkedList;
+import org.apache.commons.collections.MapIterator;
+import org.apache.commons.collections.map.MultiKeyMap;
 
 public class driver {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		File tokenFile = new File("java_lexer/test_output.tok");
-		File tableFile = new File("java_lexer/parsetable.csv");
-		String line = null;
-		//HashSet probably unnecessary if using MultiKeyMaps
-		HashSet<LinkedList<String>> table = new HashSet<LinkedList<String>>();
-		try {
-			FileReader reader = new FileReader(tokenFile);
-			BufferedReader buff = new BufferedReader(reader);
-			//This assumes there's only 1 line of tokens
-			//Glob help us all if there's more
-			line = buff.readLine();
-			String[] tokens = line.split(" ");
-			buff.close();
-			reader = new FileReader(tableFile);
-			buff = new BufferedReader(reader);
-			while((line = buff.readLine()) != null) {
-				//Remove the syso's after debugging
-				String[] rules = line.split(",");
-				for(String s: rules) {
-					System.out.print(s + " ");
-				}
-				System.out.println();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	File tokenFile = new File("test_output.tok");
+	File tableFile = new File("parsetable.csv");
+	String line = null;
+	//HashSet probably unnecessary if using MultiKeyMaps
+	//HashSet<LinkedList<String>> table = new HashSet<LinkedList<String>>();
+
+	ParseTable table;
+	try {
+	    FileReader reader = new FileReader(tokenFile);
+	    BufferedReader buff = new BufferedReader(reader);
+	    //This assumes there's only 1 line of tokens
+	    //Glob help us all if there's more
+	    line = buff.readLine();
+	    String[] tokens = line.split(" ");
+	    buff.close();
+	    reader = new FileReader(tableFile);
+	    buff = new BufferedReader(reader);
+	    String csv = "";
+	    while((line = buff.readLine()) != null) {
+		// //Remove the syso's after debugging
+		// String[] rules = line.split(",");
+		// for(String s: rules) {
+		//     System.out.print(s + " ");
+		// }
+		// System.out.println();
+		csv += line + "\n";
+	    }
+	    table = new ParseTable(csv);
+
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	    System.exit(1);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.exit(1);
 	}
+    }
 }
